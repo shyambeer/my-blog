f461b852f47315b67b946ce9509897b98c318532
Shyambeer <shyambeer@Shyambeers-MacBook-Pro.local>
2019-11-05 10:18:20 +0400

    First commit

---
+++ src/build/precache-manifest.eef248dc57981eb6dfebe2cd9d2103c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6893b9701b1878df55a501f2e34a4f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7903678efcc97aa7b3",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "1c7c26f9a831001ffd83",
+    "url": "/static/js/2.2e9b5190.chunk.js"
+  },
+  {
+    "revision": "ec7903678efcc97aa7b3",
+    "url": "/static/js/main.c1dc4f59.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
